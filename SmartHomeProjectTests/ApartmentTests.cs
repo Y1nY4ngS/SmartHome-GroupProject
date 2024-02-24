2b5bad002ecba8da5e674b85dd53a55a704e7354
Quoc Hung Nguyen <quochungnguyen1105@gmail.com>
2024-02-24 12:34:25 +0100

    unit testing start

---
+++ SmartHomeProjectTests/ApartmentTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartHomeProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeProject.Tests
+{
+    [TestClass()]
+    public class ApartmentTests
+    {
+        [TestMethod()]
+        public void ApartmentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetTempSetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetPersonInRoomTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GenerateWeatherDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
